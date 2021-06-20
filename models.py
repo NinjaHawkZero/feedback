81618252f465e02206f9ff5711310f0d99518b18
Marcus Westbrooks <marcus.westbrooks@gmail.com>
2021-06-19 23:40:15 -0400

    stuff

---
+++ models.py
@@ -0,0 +1,78 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
+
+
+
+class User(db.Model):
+    """Model for users."""
+
+    __tablename__ = "users"
+
+    username = db.Column(db.String(20), nullable=False, unique=True, primary_key=True)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+
+    feedback = db.relationship("Feedback", backref="user", cascade="all, delete")
+
+
+
+    @classmethod
+    def register(cls, username, password, first_name, last_name, email):
+        """Register a user and hash their password."""
+
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_decode = hashed.decode("utf8")
+        user = cls(
+            username=username,
+            password=hashed_decode,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+
+        db.session.add(user)
+
+        return user
+
+    
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validates user and password."""
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        else:
+            return False
+
+
+class Feedback(db.Model):
+    """Feedback."""
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(
+        db.String(20),
+        db.ForeignKey('users.username'),
+        nullable=False
+    )
